6006b2e6528fefbd986c0094d28c9a8119b69991
Michael Chop <007micle007@gmail.com>
2023-01-03 17:29:11 +0300

    Finished with the testimonial section

--- src/App.js
+++ src/App.js
@@ -131,6 +131,53 @@ const App = () => {
                     </div>
                 </div>
             </section>
+
+            <section id="testimonials">
+                <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
+                    <h2 className="text-4xl font-bold text-center">
+                        What's Different About Manage?
+                    </h2>
+                    <div className="flex flex-col mt-24 md:flex-row md:space-x-6">
+                        <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
+                            <img src="./img/avatar-anisha.png" className={"w-16 -mt-14"} alt="Anisha"/>
+                            <h5 className="text-lg font-bold">Anisha Li</h5>
+                            <p className="text-sm text-darkGrayishBlue">
+                                “Manage has supercharged our team’s workflow. The ability to
+                                maintain visibility on larger milestones at all times keeps
+                                everyone motivated.”
+                            </p>
+                        </div>
+
+                        <div
+                            className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
+                            <img src="./img/avatar-ali.png" className={"w-16 -mt-14"} alt="Anisha"/>
+                            <h5 className="text-lg font-bold">Ali Bravo</h5>
+                            <p className="text-sm text-darkGrayishBlue">
+                                “We have been able to cancel so many other subscriptions since
+                                using Manage. There is no more cross-channel confusion and
+                                everyone is much more focused.”
+                            </p>
+                        </div>
+
+                        <div
+                            className="hidden flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
+                            <img src="./img/avatar-richard.png" className={"w-16 -mt-14"} alt="Anisha"/>
+                            <h5 className="text-lg font-bold">Richard Watts</h5>
+                            <p className="text-sm text-darkGrayishBlue">
+                                “Manage has supercharged our team’s workflow. The ability to
+                                maintain visibility on larger milestones at all times keeps
+                                everyone motivated.”
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="my-16">
+                        <a href="#"
+                           className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight">Get
+                            Started</a>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
